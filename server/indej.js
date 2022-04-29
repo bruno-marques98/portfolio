f646415c4b29cc57dddbabbcd35a4c3236445edc
bruno-marques98 <bruno98silva@hotmail.com>
2022-04-29 16:54:59 +0100

    Change project verisions

---
+++ server/indej.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+// mongo123
+// mongodb+srv://admin:<password>@cluster0.zbwwa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = "mongodb+srv://admin:mongo123@cluster0.zbwwa.mongodb.net/portfolio_db?retryWrites=true&w=majority";
+ 
+const PORT = process.env.PORT|| 5000;
+ 
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log("did not connect"));
